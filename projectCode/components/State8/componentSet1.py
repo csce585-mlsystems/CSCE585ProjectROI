f1f460885db8db0c639aa5cd19e3474ffc4cc402
ArdoineDocteur <ardoinej.docteur@gmail.com>
2025-09-23 02:19:22 -0400

    Started first version of codebase. Soon, will add in code neccessary to facilitate components and routes

---
+++ projectCode/components/State8/componentSet1.py
@@ -0,0 +1,6 @@
+
+# Beginning of Components needed for <StateName>*/
+# End of Components Needed for <StateName>*/
+# Beginning of nested Components of <StateName>*/
+# End of Components for nested states of <StateName>*/
+
---
+++ projectCode/routes/State0Routes.py
@@ -0,0 +1,12 @@
+# Routes for states of <StateName>
+
+# Will insert flask code here for routes
+
+# End of Routes for states of <StateName>
+
+
+# Nested Routes for states of <StateName>
+
+
+# End of Nested Routes for states of <StateName>
+
