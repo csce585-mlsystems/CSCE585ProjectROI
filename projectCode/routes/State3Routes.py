a4e892597e184575f02838df0cac3a8e8d9bc853
ArdoineDocteur <ardoinej.docteur@gmail.com>
2025-09-23 15:49:42 -0400

    Updated codebase with placeholders to insert code. Working on placeholders for UI Components

--- projectCode/routes/State3Routes.py
+++ projectCode/routes/State3Routes.py
@@ -1,12 +1,24 @@
 # Routes for states of <StateName>
 
 # Will insert flask code here for routes
+# Route #1: <Insert desc here>
+# Body of Route: <insert psuedosteps here>
+@app.route('/<route path>') #<-- "decorator for route(argument) function"
+def route1("""Optional params if needed"""): # <-- "binding to hello_admin call"
+    # body of route
+    return # Optional Return statement
 
 # End of Routes for states of <StateName>
 
 
 # Nested Routes for states of <StateName>
 
+# Route #1: <Insert desc here>
+# Body of Route: <insert psuedosteps here>
+@app.route('/<route path>') #<-- "decorator for route(argument) function"
+def route1("""Optional params if needed"""): # <-- "binding to hello_admin call"
+    # body of route
+    return # Optional Return statement
 
 # End of Nested Routes for states of <StateName>
 
